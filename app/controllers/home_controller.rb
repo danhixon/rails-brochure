1b163eea712b07174f4f4590598ee08e1d827255
Dan Hixon <danhixon@gmail.com>
2011-04-09 14:08:53 -0700

    handle 404s as well

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
 class HomeController < ApplicationController
-  
+  def four_zero_four
+    render 'four_zero_four', :status=>404
+  end
 end
--- lib/rails-brochure/home_content.rb
+++ lib/rails-brochure/home_content.rb
@@ -1,17 +1,26 @@
 module Rails
   module Brochure
     class HomeContent
-      #TODO: replace with cattr_accessor:
-      HOME_FOLDER_PATH = "app/views/home/"
+      # This feature needs more work. 
+      # You can change the folder path and the routes are correct
+      # Rails::Brochure::HomeContent.home_folder_path = 'app/views/content/'
+      # but the home controller looks in the home folder anyway...
+      # So the home controller would need to use a different
+      # directory. If I wanted to make this configurable i'd probably
+      # ask for a controller name, define one (meta) and render the 
+      # routes accordingly.
+      @@home_folder_path = 'app/views/home/'
+      cattr_accessor :home_folder_path
+
       def self.newest
         HomeContent.files.map { |f| File.new(f) }.sort { |a,b| a.ctime <=> b.ctime }.map { |f| f.ctime }.last
       end
       def self.files
-        Dir.glob("#{HOME_FOLDER_PATH}**/*.html.*")
+        Dir.glob("#{home_folder_path}**/*.html.*")
       end
       def self.templates
         HomeContent.files.map do |f| 
-          f.gsub(/(#{HOME_FOLDER_PATH}|.html.\w+)/,"")
+          f.gsub(/(#{home_folder_path}|.html.\w+)/,"")
         end
       end
     end
